323a2e169c6252f2b431d2a3a0516270ab1406c0
bmei <meibing99@gmail.com>
2014-11-07 00:59:21 -0500

    R script for plot 1 for project 1

---
+++ ExploratoryDataAnalysis/plot_1.R
@@ -0,0 +1,27 @@
+library(data.table)
+library(lubridate)
+
+# get column classes to speed up data file reading
+tab5rows <- fread("household_power_consumption.txt", sep=";", header=TRUE, nrows=5, stringsAsFactors=FALSE, na.strings="?")
+classes <- sapply(tab5rows, class)
+
+# read all data
+DF <- read.table("household_power_consumption.txt", sep=";", header=TRUE, stringsAsFactors=FALSE, na.strings="?", colClasses=classes)
+DT <- data.table(DF)
+DF = NULL           # free up some space
+
+# slice the data to get the target ones
+setkey(DT, Date)
+myDT <- DT[c("1/2/2007","2/2/2007")]
+
+# change the class and format of Date
+myDT$Date <- as.Date(myDT$Date, "%d/%m/%Y")
+
+# form the datetime column by combining the Date and Time columns
+# then use the ymd_hms() function in the lubridate package to convert the class of datetime
+myDT[, datetime:=ymd_hms(paste(myDT$Date, myDT$Time, sep=" "))]
+
+# plot #1
+png("plot_1.png", width=480, height=480, units="px")
+hist(myDT$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
